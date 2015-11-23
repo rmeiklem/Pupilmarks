a031bb7358b785a3f2948d1ab7a48d90431b06a1
robbie <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-23 11:19:47 +0000

    spring 1.1

---
+++ Club.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Club here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Club
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Club
+     */
+    public Club()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
