d1adc6de70441b8bec5fc03928706a5f3346c5e3
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-11-24 09:19:08 +0000

    deleting code

--- Member.java
+++ Member.java
@@ -1,10 +1,5 @@
 
-/**
- * Write a description of class Member here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Member
 {
     // instance variables - replace the example below with your own
