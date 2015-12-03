6cf4d8a897fc776ee128675efe7ddb2eebb7028a
Guest User <Guest@stk-cs01-ws13.westlothian.sch.uk>
2015-12-03 11:16:17 +0000

    sprint 3.0

--- School.java
+++ School.java
@@ -20,11 +20,18 @@ public class School
     { 
         System.out.println("top mark: top mark update");
         System.out.println("** preparing to read data file.");
-         
+
         String[] datarows = fILEREAD.readCSVtable();
         noOfMembers = datarows.length - 1;
-        
+
         System.out.println("** " + noOfMembers + " rows read.\n\n");
+
+        memberlist = new Member[noOfMembers];
+
+        for (int i = 0; i < noOfMembers; i++) {
+            memberlist[i] = new Member();
+            memberlist[i].readMemberDetails(datarows[i+1]);
+        }
     }
 
     public void Topmark()
